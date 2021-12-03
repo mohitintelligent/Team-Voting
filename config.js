75121b0e7d5d07582d9d215d193ba7e9e0a10f88
mohitintelligent <mmaliwal2409@gmail.com>
2021-12-03 14:03:09 +0530

    Changed config.js

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyBgAwhVmWSxvBHl_WBCD0aUizeLk0EYxLQ",
+    authDomain: "hotairballoon-74443.firebaseapp.com",
+    databaseURL: "https://hotairballoon-74443-default-rtdb.firebaseio.com",
+    projectId: "hotairballoon-74443",
+    storageBucket: "hotairballoon-74443.appspot.com",
+    messagingSenderId: "994078948835",
+    appId: "1:994078948835:web:83d3f315f4668ca0d934d6"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
